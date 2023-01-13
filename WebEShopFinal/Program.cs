251fd29514e98ffca565ab62a1fe3bcf7edabf3f
David Oster aka George Pasparakis <paspa@hotmail.com>
2023-01-13 12:02:20 +0200

    MVC

--- WebEShopFinal/Program.cs
+++ WebEShopFinal/Program.cs
@@ -19,6 +19,8 @@ namespace WebEShopFinal
             builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddRazorPages();
+            builder.Services.AddMvc();
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
